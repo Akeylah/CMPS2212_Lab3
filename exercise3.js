7c48e6a0d2f27dcad1d155fd484b9429a62fd506
Akeylah <112662745+Akeylah@users.noreply.github.com>
2026-02-14 01:48:55 +0000

    Complete Exercise 3: The Object Context Challenge

---
+++ exercise3.js
@@ -0,0 +1,21 @@
+const display = document.querySelector('#count-display');
+const CounterApp = {
+count: 0,
+increment: function() {
+// increment this.count, update the display span
+    this.count++;
+    display.textContent = this.count;
+},
+decrement: function() {
+// decrement this.count, update the display span
+    this.count--;
+    display.textContent = this.count;
+}
+};
+// Attach increment to #inc-btn using .bind()
+const inc_Btn = document.querySelector('#inc-btn');
+inc_Btn.addEventListener('click', CounterApp.increment.bind(CounterApp));
+
+// Attach decrement to #dec-btn using .bind()
+const dec_Btn = document.querySelector('#dec-btn');
+dec_Btn.addEventListener('click', CounterApp.decrement.bind(CounterApp));
