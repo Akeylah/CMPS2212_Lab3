43c8fc5119691afa2b68bd72a1836846dc67c8dc
Akeylah <112662745+Akeylah@users.noreply.github.com>
2026-02-14 01:33:15 +0000

    Complete Exercise 2: The Data Attribute Reader

---
+++ exercise2.js
@@ -0,0 +1,12 @@
+const swatches = document.querySelectorAll('.swatch');
+// Define a function that reads this.dataset.color
+// and applies it as this.style.backgroundColor
+// Then attach it to every swatch
+function colorchange() {
+    const color = this.dataset.color;
+    this.style.backgroundColor = color;
+}
+
+swatches.forEach(function(swatch) {
+    swatch.addEventListener('click', colorchange);
+});
